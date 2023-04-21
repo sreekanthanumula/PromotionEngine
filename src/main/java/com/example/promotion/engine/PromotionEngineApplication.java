3ad9a3a1af4b42d1d59a381f06da85041a1508e4
sreekanth <srithbits@gmail.com>
2023-04-21 12:18:10 +0100

    Spring Initializer Project from Spring.IO - Intial commit with project skeleton

---
+++ src/main/java/com/example/promotion/engine/PromotionEngineApplication.java
@@ -0,0 +1,13 @@
+package com.example.promotion.engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromotionEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromotionEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/promotion/engine/PromotionEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.promotion.engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromotionEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
